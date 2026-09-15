e347c5a159ac66ae3ffbabc84c3e597c8426bb13
Matheus Lacet <matheus.lacet@fernandapessoa.com.br>
2026-09-15 18:32:31 -0300

    Revert "Make GitHub Pages branch deployment work"
    This reverts commit 63b59155b60cb55ab3377cb2e7ae0b9b4f5a095b.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	base: '/report-semanal/',
-	root: 'src',
-	publicDir: '../public',
-	build: { outDir: '../dist', emptyOutDir: true },
 	plugins: [react()],
 });
