63b59155b60cb55ab3377cb2e7ae0b9b4f5a095b
Matheus Lacet <matheus.lacet@fernandapessoa.com.br>
2026-09-15 17:39:27 -0300

    Make GitHub Pages branch deployment work

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	base: '/report-semanal/',
+	root: 'src',
+	publicDir: '../public',
+	build: { outDir: '../dist', emptyOutDir: true },
 	plugins: [react()],
 });
